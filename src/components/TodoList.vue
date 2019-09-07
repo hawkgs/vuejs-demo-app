5e631806ed8e92997adb6a2b13c9b44b95563108
hawkgs <hawk.dvp@gmail.com>
2019-09-07 12:35:12 +0300

    Style todo items

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -29,6 +29,9 @@ export default {
 
 <style scoped>
   .todo-list {
-    border: 1px solid red;
+    width: 600px;
+    margin: 50px auto 0 auto;
+    background: white;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
   }
 </style>
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,6 +1,13 @@
 <template>
-  <div v-bind:class="{ starred }">
-    <slot></slot>
+  <div class="todo-list-item">
+    <p v-bind:class="{ 'text': true, starred }">
+      <span v-if="starred">⭐</span>
+      <slot></slot>
+    </p>
+    <div class="actions">
+      <span>{{ starred ? 'Unstar' : 'Star' }}</span>
+      <span>Done</span>
+    </div>
   </div>
 </template>
 
@@ -15,7 +22,38 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .starred {
+  .todo-list-item {
+    border-top: 1px solid #eee;
+    padding: 15px 20px;
+    box-sizing: border-box;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+  }
+
+  .todo-list-item:hover {
+    background-color: rgb(245, 245, 245);
+  }
+
+  .text {
+    flex: 1;
+  }
+
+  .text.starred {
     color: goldenrod;
+  }
+
+  .actions {
+    font-size: 14px;
+    color: #999;
+    cursor: pointer;
+  }
+
+  .actions > span {
+    margin-left: 10px;
+  }
+
+  .actions > span:hover {
+    text-decoration: underline;
   }
 </style>
